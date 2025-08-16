552a47eaa533060bc77b50ac01925404772091d0
Maurício Henrique <sixtyfouroftheclock@proton.me>
2025-08-16 11:58:55 -0300

    Hotfix
    Changed project version to 8
    Fixed Logger debug mode
    Separated console structs
    Removed set_debug
    Added debug_mode test

--- include/recurring/console/logger.hpp
+++ include/recurring/console/logger.hpp
@@ -3,42 +3,39 @@
 #include <print>
 #include <recurring/console/card.hpp>
 #include <recurring/utils/api.hpp>
+#include <recurring/utils/string.hpp>
 
 namespace Recurring::Console
 {
     class RLIB Logger
     {
-        static inline bool debug_mode = false;
 
     public:
-        static constexpr Card WARNING
-            = { Card::Color::YELLOW, Card::Title::WARNING };
-        static constexpr Card INFO = { Card::Color::GREEN, Card::Title::INFO };
-        static constexpr Card ERROR = { Card::Color::RED, Card::Title::ERROR };
-        static constexpr Card DEBUG
-            = { Card::Color::GRAY, Card::Title::DEBUGGING };
-        static constexpr Card GENERIC
-            = { Card::Color::WHITE, Card::Title::GENERIC };
+        static inline bool debug_mode = false;
 
-        static void set_debug (bool mode);
+        // The "kind" of any println. It's gonna be putted on the left side.
+        static constexpr Card WARNING = { Color::YELLOW, Title::WARNING };
+        static constexpr Card INFO = { Color::GREEN, Title::INFO };
+        static constexpr Card ERROR = { Color::RED, Title::ERROR };
+        static constexpr Card DEBUG = { Color::GRAY, Title::DEBUGGING };
+        static constexpr Card GENERIC = { Color::WHITE, Title::GENERIC };
 
         template <typename... Args>
         static void
         print (const char* description, Args&&... args)
         {
-            std::print (
-                "{}\n",
-                std::vformat (description, std::make_format_args (args...)));
+            std::print ("{}\n", std::vformat (description, std::make_format_args (args...)));
         }
 
         template <typename... Args>
         static void
         print (const Card& card, const char* description, Args&&... args)
         {
-            std::print (
-                "[{2}{0}{3}] {1}\n", card.title,
-                std::vformat (description, std::make_format_args (args...)),
-                card.color, Card::Color::RESET);
+            if (!debug_mode && String::compare (card.title, DEBUG.title))
+            {
+                return;
+            }
+            std::print ("[{2}{0}{3}] {1}\n", card.title, std::vformat (description, std::make_format_args (args...)), card.color, Color::RESET);
         }
     };
 } // namespace Recurring::Console
